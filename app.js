3d0daf2ba10a203e77fb199d5d4e0308ac35912a
Adarsh Pathange <adarsh.patz@gmail.com>
2023-11-27 20:03:13 -0600

    app.js

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ $(document).ready(function() {
         searchLocation();
     });
 
+    $('#useCurrentLocation').on('click', function() {
+        getCurrentLocation();
+    });
+
     function searchLocation() {
         const location = $('#searchLocation').val();
         const apiKey = 'AIzaSyBwAMKFRjZPRybeLrNOIHX9NrAK-U2l7vo';
@@ -29,9 +33,25 @@ $(document).ready(function() {
         });
     }
 
+    function getCurrentLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                const latitude = position.coords.latitude;
+                const longitude = position.coords.longitude;
+
+                // Get sunrise and sunset data using the obtained coordinates
+                getSunriseSunset(latitude, longitude);
+            }, function(error) {
+                displayError("Error getting current location.");
+            });
+        } else {
+            displayError("Geolocation is not supported by this browser.");
+        }
+    }
+
     function getSunriseSunset(latitude, longitude) {
         // Sunrise Sunset API request
-        const apiUrl = `https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}`;
+        const apiUrl = `https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}&formatted=0`;
 
         $.ajax({
             url: apiUrl,
@@ -57,18 +77,20 @@ $(document).ready(function() {
             <p><strong>Sunset:</strong> ${result.sunset}</p>
             <p><strong>Dawn:</strong> ${result.dawn}</p>
             <p><strong>Dusk:</strong> ${result.dusk}</p>
-            <p><strong>Day Length:</strong> ${result.day_length}</p>
+            <p><strong>Day Length:</strong> ${result.day_length} seconds</p>
             <p><strong>Solar Noon:</strong> ${result.solar_noon}</p>
+            <p><strong>Timezone:</strong> ${result.timezone}</p>
         `);
 
         // Display tomorrow information
         $('#tomorrowResult').html(`
-        <p><strong>Sunrise:</strong> ${result.sunrise}</p>
-        <p><strong>Sunset:</strong> ${result.sunset}</p>
-        <p><strong>Dawn:</strong> ${result.dawn}</p>
-        <p><strong>Dusk:</strong> ${result.dusk}</p>
-        <p><strong>Day Length:</strong> ${result.day_length}</p>
-        <p><strong>Solar Noon:</strong> ${result.solar_noon}</p>
+            <p><strong>Sunrise:</strong> ${result.sunrise}</p>
+            <p><strong>Sunset:</strong> ${result.sunset}</p>
+            <p><strong>Dawn:</strong> ${result.dawn}</p>
+            <p><strong>Dusk:</strong> ${result.dusk}</p>
+            <p><strong>Day Length:</strong> ${result.day_length} seconds</p>
+            <p><strong>Solar Noon:</strong> ${result.solar_noon}</p>
+            <p><strong>Timezone:</strong> ${result.timezone}</p>
         `);
     }
 
